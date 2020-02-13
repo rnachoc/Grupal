e8456a56017da290d19f6b9b88d432da32f7d997
josechucuyan <josedachu@gmail.com>
2020-02-13 11:53:11 +0100

    He creado la función de aumentar el sueldo.

--- src/Entornos_Desarrollo_Tema_8_9/ITrabajador.java
+++ src/Entornos_Desarrollo_Tema_8_9/ITrabajador.java
@@ -4,14 +4,19 @@ public interface ITrabajador {
 	
 	// INTERFAZ ITRABAJADOR TERMINADA
 	
+	//Estos los metodos get y set de la variable sueldo
 	public int getSueldo();
 	
 	public void setSueldo(int iSueldo);
 	
+	
+	//Estos los metodos get y set de la variable antiguedad
 	public int getAntiguedad();
 	
 	public void setAntiguedad(int antiguedad);
 	
+	
+	//Funcoin para aumentar el sueldo
 	public void subirSueldo(int porcentaje);
 	
 }
